37dda543c401415883f17d5558abfc3f07e3bfaa
tim <t.p.maerov@tinkoff.ru>
2023-02-27 14:39:45 +0300

    fix

--- src/test/kotlin/com/tinkoff/backend/database/ConnectionTest.kt
+++ src/test/kotlin/com/tinkoff/backend/database/ConnectionTest.kt
@@ -1,13 +1,14 @@
 package com.tinkoff.backend.database
 
 import java.sql.DriverManager
+import org.h2.Driver
 import org.junit.jupiter.api.Test
 
 class ConnectionTest {
 
 
     @Test
-    fun create() {
+    fun createPostgres() {
         /*
             docker run -d --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 postgres
             docker exec -it postgres-test psql -U postgres
@@ -17,13 +18,24 @@ class ConnectionTest {
 
 //        val connection = DriverManager.getConnection("jdbc:tc:postgresql:11:///test")
 
-//        val connection = DriverManager.getConnection("jdbc:h2:mem:testdb")
 
         val sql = "SELECT 4*4;"
         val result = connection.prepareStatement(sql).executeQuery()
         result.next()
         println(result.getInt(1))
     }
+
+    @Test
+    fun createH2() {
+        val connection = Driver().connect("jdbc:h2:mem:testdb", null)
+
+        val sql = "SELECT 4*4;"
+        val result = connection.prepareStatement(sql).executeQuery()
+        result.next()
+        println(result.getInt(1))
+    }
+
+
 
 
 }
